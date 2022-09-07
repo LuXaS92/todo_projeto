a623cc4136526908719fe4ebcdc30bb8ae955f4a
LuXaS92 <109088944+LuXaS92@users.noreply.github.com>
2022-09-07 14:06:50 -0300

    commit 4

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,5 @@
 class TasksController < ApplicationController
-  before_action :set_task, only: %i[ :edit, :update, :destroy ]
+  before_action :set_task, only: %i[ edit update destroy ]
 
   def index
     @task = Task.all
