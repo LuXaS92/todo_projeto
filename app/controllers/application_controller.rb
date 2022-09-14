7ea8fbdcd0cd42e957c0df21dbc064d85353ad18
LuXaS92 <109088944+LuXaS92@users.noreply.github.com>
2022-09-14 14:51:36 -0300

    commit 14

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+  before_action :authenticate
+
+  private
+
+  def authenticate
+    authenticate_or_request_with_http_basic do |username, password|
+      username == 'admin' && password ''
+  end
+
 end
